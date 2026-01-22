ea016071f210385db2ca5b406656e7e7942a4b22
DeepighaJ <jmdeepigha@gmail.com>
2026-01-21 16:58:43 -0800

    Day1 Javascript variable home assignment

---
+++ Week1/Day1.js
@@ -0,0 +1,16 @@
+let userName = "Deepigha123"
+let organization = "TechnoElevate"
+let contactNumber = 9876543210
+let knowsAutomation = true
+let usesPlaywright 
+
+
+console.log("User Name: " ,userName)
+console.log("Organization: " ,organization)
+console.log("Contact Number: " , contactNumber)
+console.log("Knows Automation: " , knowsAutomation)
+console.log("Uses Playwright: " , usesPlaywright)
+
+onsole.log("The typeOf Contact Number: " , typeof(contactNumber))
+console.log("The typeOf Knows Automation: " , typeof(knowsAutomation))
+console.log("The typeOf Uses Playwright: " , typeof(usesPlaywright))
